8c656a956a81ce764aa4f0623ea9633ca4177d86
mostafaALBASH <mostafa_elbesh@outlook.com>
2023-04-03 12:22:23 +0200

    task-4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,17 +3,17 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://f91orpk2aa.execute-api.us-east-1.amazonaws.com/dev',
+    order: 'https://f91orpk2aa.execute-api.us-east-1.amazonaws.com/dev',
+    import: 'https://f91orpk2aa.execute-api.us-east-1.amazonaws.com/dev',
+    bff: 'https://f91orpk2aa.execute-api.us-east-1.amazonaws.com/dev',
+    cart: 'https://f91orpk2aa.execute-api.us-east-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
     import: false,
-    bff: false,
+    bff: true,
     cart: false,
   },
 };
